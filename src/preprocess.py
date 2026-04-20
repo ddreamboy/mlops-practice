7306b984a1400a32592ab11385eb2dff465f1ac2
ddreamboy <senin.arkadiy@gmail.com>
2026-04-20 19:42:35 +0300

    fix: improve preprocess method to handle bytes input

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,11 @@
+import json
+
+
 class Preprocess:
-    def preprocess(self, body: dict, state: dict, collect_custom_statistics_fn=None):
-        return body.get("text", "")
+    def preprocess(self, body, state: dict, collect_custom_statistics_fn=None):
+        if isinstance(body, bytes):
+            body = json.loads(body)
+        return [body.get("text", "")]
 
     def postprocess(self, data, state: dict, collect_custom_statistics_fn=None):
         label = data[0] if hasattr(data, "__iter__") else data
